0b0abaa1021dc7e18524ded39b887b1a5325a311
vityapain <example@gmail.com>
2022-02-09 13:22:59 +0300

    practice_1

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 "use strict";
 
-let inc = 10,
-    decr = 10;
+// first task
+let numberOfFilms = +prompt('How many books do you read?',"1");
 
-// inc++;
-console.log(inc++);
-console.log(inc);
+// second task
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+// third task
+const a = prompt('Last film watched you',''),
+    c = prompt('Point',''),
+    b = prompt('Last film watched you',''),
+    d = prompt('Point','');
+
+// personalMovieDB.movies = {
+//     a: c,
+//     b: d
+// };
+personalMovieDB.movies[a] = c;
+personalMovieDB.movies[b] = d;
+    
+console.log(personalMovieDB);
+    
