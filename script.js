853862f4c53cf98dc5046a8391983b12107c77b4
vityapain <example@gmail.com>
2022-02-12 22:44:55 +0300

    practice_func

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 "use strict";
 
-// first task
-let numberOfFilms = +prompt('How many books do you read?',"1");
+let numberOfFilms;
 
-// second task
-let personalMovieDB = {
+function start() {
+    numberOfFilms = +prompt("Skol`ko filmov vi yzhe posmotreli?",'');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Skol`ko filmov vi yzhe posmotreli?",'');
+    }
+}
+
+start();
+
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
@@ -12,20 +20,55 @@ let personalMovieDB = {
     privat: false
 };
 
-alert("Ya enlan");
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt("Last film?",''),
+                b = prompt('Point?','');
+        
+        if (a != null && b != null & a != '' && b != '' && a.length <50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
 
-// third task
-const a = prompt('Last film watched you',''),
-    c = prompt('Point',''),
-    b = prompt('Last film watched you',''),
-    d = prompt('Point','');
+rememberMyFilms();
 
-// personalMovieDB.movies = {
-//     a: c,
-//     b: d
-// };
-personalMovieDB.movies[a] = c;
-personalMovieDB.movies[b] = d;
-    
-console.log(personalMovieDB);
-    
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Little');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Normal');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Hype');
+    } else {
+        console.log('error!');
+    }
+}
+
+detectPersonalLevel();
+
+
+function writeYourGenres() {
+    for (let i = 0; i < 3; i++) {
+        personalMovieDB.genres[i] = prompt(`Favourite genres${i+1}?`,"");
+        if (personalMovieDB.genres[i] == '' || personalMovieDB.genres[i] == null) {
+            i--;
+        }
+    }
+}
+
+writeYourGenres();
+
+function showMyDB() {
+    if (personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
+    } else {
+        console.log('Acces error!');
+    }
+}
+showMyDB();
+// console.log(personalMovieDB);
